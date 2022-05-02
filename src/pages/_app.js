4870f02cd1a76bdd171d908a2b643bdb9ec3b732
Amovane <amovane@163.com>
2022-05-02 13:59:16 +0800

    Add logo & change favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,17 @@
 /* pages/_app.js */
 import "../styles/globals.css";
 import { NavItem } from "../components/navitem";
+import Image from "next/image";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="px-8">
-      <nav className="navbar py-4 bg-base-100">
+      <nav className="navbar py-4 px-4 bg-base-100">
         <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-2xl font-bold">AmoNFT</a>
+          <Image src="/logo.png" width={35} height={35} alt="logo" />
+          <a className="btn btn-ghost normal-case text-2xl font-bold">
+            NinjaNFT
+          </a>
         </div>
         <ul className="menu menu-horizontal p-0">
           <NavItem href="/" title="Home" />
